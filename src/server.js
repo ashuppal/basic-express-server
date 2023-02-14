1ab387fcaf22e65c7deab54358fbdf7c24370767
Ash Uppal <ashuppal@gmail.com>
2023-02-14 15:20:17 -0800

    all tests passing

--- src/server.js
+++ src/server.js
@@ -1,42 +1,31 @@
 'use strict';
 
-const express = require('express');
-const app = express();
+const express = require ('express');
+const app = express ();
 const port = process.env.PORT || 3001;
-const errorHandler = require('./error-handlers/500.js');
-const logger = require('./middleware/logger.js');
-const notFound = require('./error-handlers/404.js');
+const errorHandler = require ('./error-handlers/500.js');
+const logger = require ('./middleware/logger.js');
+const notFound = require ('./error-handlers/404.js');
 
 function start () {
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  app.listen (port, () => {
+    console.log (`Server listening on port ${port}`);
   });
 }
 
-app.get('/', logger,(req, res,next) => {
-  res.status(200).send('Hello there ');
+app.get ('/', logger, (req, res, next) => {
+  res.status (200).send ('Hello there ');
 });
 
-app.get('/person', (req, res,next) => {
-  if(!req.query.name){
-    next('No name provided');
-  }  
-  res.status(200).send(`Name: ${req.query.name}`);
+app.get ('/person', (req, res, next) => {
+  if (!req.query.name) {
+    next ('No name provided');
+  }
+  res.status (200).send (`Hello there : ${req.query.name}`);
 });
 
-
-
- 
-
-app.use('*', notFound);
-app.use(errorHandler);
-app.use(logger);
-
-
-
+app.use ('*', notFound);
+app.use (errorHandler);
+app.use (logger);
 
 module.exports = {start, app};
-
-  
-
-
